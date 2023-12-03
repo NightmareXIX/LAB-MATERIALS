69068b1353062ad1453cf5becdfb78fb45bd2bd0
Sadnan <binaislam321@gmail.com>
2023-12-04 00:12:35 +0600

    Add files via upload

---
+++ DATA STRUCTURES AND ALGORITHMS/ASSIGNMENT 4/problem2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cin >> arr[i][j];
+    }
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i][j] == 0 && arr[i][k] == 1 && arr[k][j] == 1)
+                    arr[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+}
